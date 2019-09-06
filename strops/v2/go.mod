50bb2137873448cbdf82b9aebfc6000f58409032
Mike Rapp <mike@paladinacs.net>
2019-09-05 21:58:13 -0500

    Fixed test TestStrOps_ExtractDataField_02()

--- strops/v2/go.mod
+++ strops/v2/go.mod
@@ -1,3 +1,5 @@
 module github.com/MikeAustin71/stringopsgo/strops/v2
 
 go 1.12
+
+require golang.org/x/tools v0.0.0-20190905235650-93dcc2f048f5 // indirect
--- strops/v2/xt_strops_02_test.go
+++ strops/v2/xt_strops_02_test.go
@@ -37,7 +37,6 @@ func TestStrOps_FindLastWord_14(t *testing.T) {
 
 }
 
-
 func TestStrOps_ExtractDataField_01(t *testing.T) {
 
   endOfLineDelimiters := []string{"\n"}
@@ -56,7 +55,6 @@ func TestStrOps_ExtractDataField_01(t *testing.T) {
     "\v",
     " "}
 
-  //("\t \r\f\n\v")
   targetStr := " Zone:\t America/Chicago\t Good morning America!\n"
   lenTargetStr := len(targetStr)
   expectedLastGoodIdx := strings.LastIndex(targetStr, "\n")
@@ -198,24 +196,42 @@ func TestStrOps_ExtractDataField_01(t *testing.T) {
 
 }
 
-/*
 func TestStrOps_ExtractDataField_02(t *testing.T) {
 
-  endOfLineRunes := []rune("\n#")
-  leadingRunes := []rune("\t \r\f\n\v")
-  trailingRunes := []rune("\t \r\f\n\v")
+  endOfLineDelimiters := []string{"\n"}
+
+  commentDelimiters := []string{"#"}
+
+  leadingFieldDelimiters := []string{
+    "\t",
+    "\r",
+    "\f",
+    "\v",
+    " "}
+
+  trailingFieldDelimiters := []string{
+    "\t",
+    "\r",
+    "\f",
+    "\v",
+    " "}
+
   targetStr := " Zone:\t America/Chicago Good morning America!\n"
   lenTargetStr := len(targetStr)
   expectedLastGoodIdx := strings.LastIndex(targetStr, "\n")
+  expectedEndOfLineDelimiterIdx := expectedLastGoodIdx
   expectedLastGoodIdx--
   startIdx := 0
   leadingKeyWordDelimiter := "Zone:"
+  expectedLeadingKeyWordDelimiterIndex := strings.Index(targetStr, leadingKeyWordDelimiter)
   expectedDataFieldStr := "America/Chicago"
   expectedDataFieldIdx := strings.Index(targetStr, expectedDataFieldStr)
   expectedDataFieldLength := len(expectedDataFieldStr)
-  expectedLeadingKeyWordDelimiterIndex := strings.Index(targetStr, leadingKeyWordDelimiter)
-  expectedDataFieldTrailingDelimiter := ' '
+  expectedDataFieldTrailingDelimiter := " "
   expectedDataFieldTrailingDelimiterType := DfTrailDelimiter.EndOfField()
+  expectedEndOfLineDelimiter := "\n"
+  expectedCommentDelimiter := ""
+  expectedCommentDelimiterIndex := -1
 
   expectedNextTargetIdx := expectedDataFieldIdx + expectedDataFieldLength
 
@@ -229,9 +245,10 @@ func TestStrOps_ExtractDataField_02(t *testing.T) {
     targetStr,
     leadingKeyWordDelimiter,
     startIdx,
-    leadingRunes,
-    trailingRunes,
-    endOfLineRunes)
+    leadingFieldDelimiters,
+    trailingFieldDelimiters,
+    commentDelimiters,
+    endOfLineDelimiters)
 
   if err != nil {
     t.Errorf("Error returned by StrOps{}.ExtractDataField()\n"+
@@ -311,8 +328,36 @@ func TestStrOps_ExtractDataField_02(t *testing.T) {
       "Instead, datDto.NextTargetStrIndex='%v'.\n",
       expectedNextTargetIdx, datDto.NextTargetStrIndex)
   }
+
+  if expectedEndOfLineDelimiter != datDto.EndOfLineDelimiter {
+    t.Errorf("ERROR: Expected datDto.EndOfLineDelimiter='%v'.\n"+
+      "Instead, datDto.EndOfLineDelimiter='%v'.\n",
+      StrOps{}.ConvertNonPrintableCharacters([]rune(expectedEndOfLineDelimiter), false),
+      StrOps{}.ConvertNonPrintableCharacters([]rune (datDto.EndOfLineDelimiter), false))
+  }
+
+  if expectedEndOfLineDelimiterIdx != datDto.EndOfLineDelimiterIndex  {
+    t.Errorf("ERROR: Expected datDto.EndOfLineDelimiterIndex='%v'.\n"+
+      "Instead, datDto.EndOfLineDelimiterIndex='%v'.\n",
+      expectedEndOfLineDelimiterIdx, datDto.EndOfLineDelimiterIndex)
+  }
+
+  if expectedCommentDelimiter != datDto.CommentDelimiter {
+    t.Errorf("ERROR: Expected datDto.CommentDelimiter='%v'.\n"+
+      "Instead, datDto.CommentDelimiter='%v'.\n",
+      StrOps{}.ConvertNonPrintableCharacters([]rune(expectedCommentDelimiter), true),
+      StrOps{}.ConvertNonPrintableCharacters([]rune (datDto.CommentDelimiter), true))
+  }
+
+  if expectedCommentDelimiterIndex != datDto.CommentDelimiterIndex  {
+    t.Errorf("ERROR: Expected datDto.CommentDelimiterIndex='%v'.\n"+
+      "Instead, datDto.CommentDelimiterIndex='%v'.\n",
+      expectedEndOfLineDelimiterIdx, datDto.CommentDelimiterIndex)
+  }
+
 }
 
+/*
 func TestStrOps_ExtractDataField_03(t *testing.T) {
 
   endOfLineRunes := []rune("\n#")
